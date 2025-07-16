ca8220e42f96ba124bfaa1373982ba81a87012cc
Edd13Garc1a <143624401+Edd13Garc1a@users.noreply.github.com>
2025-07-16 21:08:45 +0300

    Update install_elk.sh
    Local repo

--- scripts/install_elk.sh
+++ scripts/install_elk.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 set -e
 
-echo "[+] Скачиваем и устанавливаем Elasticsearch, Kibana, Logstash, Filebeat (v7.17.13)"
+DEB_DIR="/home/odmin/elk"
 
-cd /tmp
+echo "[1/6] Проверка наличия .deb пакетов в $DEB_DIR"
+if [ ! -d "$DEB_DIR" ]; then
+  echo "❌ Папка $DEB_DIR не найдена. Помести туда .deb файлы ELK!" >&2
+  exit 1
+fi
 
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.17.13-amd64.deb
-wget https://artifacts.elastic.co/downloads/kibana/kibana-7.17.13-amd64.deb
-wget https://artifacts.elastic.co/downloads/logstash/logstash-7.17.13-amd64.deb
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.17.13-amd64.deb
+cd "$DEB_DIR"
 
-dpkg -i elasticsearch-7.17.13-amd64.deb
-dpkg -i kibana-7.17.13-amd64.deb
-dpkg -i logstash-7.17.13-amd64.deb
-dpkg -i filebeat-7.17.13-amd64.deb
+echo "[2/6] Устанавливаем .deb пакеты Elastic Stack 8.9.1"
+sudo dpkg -i elasticsearch-8.9.1-amd64.deb \
+             kibana-8.9.1-amd64.deb \
+             logstash-8.9.1-amd64.deb \
+             filebeat-8.9.1-amd64.deb || sudo apt install -f -y
 
-echo "[+] Конфигурируем Elasticsearch"
-sed -i 's/#network.host: .*/network.host: localhost/' /etc/elasticsearch/elasticsearch.yml
-systemctl enable elasticsearch
-systemctl start elasticsearch
+echo "[3/6] Отключаем X-Pack Security (Elasticsearch)"
+sudo bash -c "echo '
+network.host: localhost
+xpack.security.enabled: false
+' >> /etc/elasticsearch/elasticsearch.yml"
 
-echo "[+] Конфигурируем Kibana"
-sed -i 's/#server.host: .*/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml
-systemctl enable kibana
-systemctl start kibana
+sudo systemctl enable elasticsearch
+sudo systemctl start elasticsearch
 
-echo "[+] Копируем logstash.conf"
-cp logstash.conf /etc/logstash/conf.d/logstash.conf
-systemctl enable logstash
-systemctl restart logstash
+echo "[4/6] Конфигурируем Kibana"
+sudo bash -c "echo '
+server.host: \"0.0.0.0\"
+elasticsearch.hosts: [\"http://localhost:9200\"]
+elasticsearch.ssl.verificationMode: none
+' >> /etc/kibana/kibana.yml"
 
-echo "[+] Настройка Filebeat"
-cp filebeat.yml /etc/filebeat/filebeat.yml
-filebeat modules enable nginx mysql
-systemctl enable filebeat
-systemctl restart filebeat
+sudo systemctl enable kibana
+sudo systemctl start kibana
 
-echo "[✔] ELK стек успешно установлен из .deb"
+echo "[5/6] Настраиваем Logstash"
+sudo cp ~/linux_restore_scripts/logstash.conf /etc/logstash/conf.d/logstash.conf
+sudo systemctl enable logstash
+sudo systemctl restart logstash
+
+echo "[6/6] Настраиваем Filebeat"
+sudo cp ~/linux_restore_scripts/filebeat.yml /etc/filebeat/filebeat.yml
+sudo filebeat modules enable nginx mysql
+sudo systemctl enable filebeat
+sudo systemctl restart filebeat
+
+echo "[✔] Elastic Stack 8.9.1 установлен и запущен (без авторизации)"
